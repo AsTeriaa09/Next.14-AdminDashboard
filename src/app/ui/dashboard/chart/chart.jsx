bef435cdff8c7646e046a2f61aa58e1f1dc2eae5
AsTeriaa09 <mahiraakhter950@gmail.com>
2025-01-07 16:47:47 +0600

    add: create dashboard and add styling

---
+++ src/app/ui/dashboard/chart/chart.jsx
@@ -0,0 +1,8 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./chart.module.css";
+
+const Chart = ({ }) => {
+  return <></>;
+};
+
+export default Chart;
---
+++ src/app/ui/dashboard/rightbar/rightbar.jsx
@@ -0,0 +1,8 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
+import styles from "./rightbar.module.css";
+
+const Rightbar = ({ }) => {
+  return <></>;
+};
+
+export default Rightbar;
