13d6a084339cf2244ff86b264ca35c03989fbebc
AsTeriaa09 <mahiraakhter950@gmail.com>
2025-01-08 12:13:51 +0600

    add: connect to db

---
+++ src/app/lib/utils.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connection = {};
+
+export const connectToDb = async () => {
+  try {
+    if (connection.isConnected) {
+      return;
+    }
+    const db = await mongoose.connect(process.env.MONGO_URL);
+    connection.isConnected = db.connection[0].readyState;
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
